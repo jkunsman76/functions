fc55348155cedc4d3cc5f4f48c17510c1995f0cb
jkuns76 <jkoonman@gmail.com>
2021-10-11 11:10:52 -0500

    new repo

---
+++ practice.js
@@ -0,0 +1,86 @@
+// const calculateAgeInDogYears = (ageOfPerson) => {
+//     const ageInDogYears = ageOfPerson / 7
+
+//     return ageInDogYears
+// }
+
+// console.log(calculateAgeInDogYears(22))
+
+
+
+
+// const dogFunc = (dogParameter) => {
+//     const favBreed = `My favorite dog breed is ${dogParameter}`
+//     if (dogParameter == 'meow') {
+// return "I like cats"
+//     } 
+
+// return favBreed 
+
+// }
+
+// const favDogBreed = dogFunc("meow")
+// console.log(`When it comes to pets, ${favDogBreed}`)
+
+
+// const add = (num2, num3, num1) => {
+//     let sum = (num1 + num2 + num3)
+//     return sum
+// }
+// console.log(add(11, 17, 4))
+
+// const go = (direction,speed) => {
+//     const message = `The car is moving ${direction} at ${speed}`
+//     if (speed > 75) {
+//     return message + ' SLOW DOWN'
+//     }
+//     return message
+// }
+// console.log(go("forward", 85))
+
+// const arrayOfNumbers = [11,29,36,4,59,67,76,0]
+// const evenOrOdd = (num) => {
+//     if (num % 2 === 0) {
+//         return "Even"
+//     }
+//     return "Odd"
+// }
+// console.log(evenOrOdd(11))
+// for (const numObject of arrayOfNumbers) {
+//     console.log(evenOrOdd(numObject))
+// }
+
+
+// const words = [
+//     "The", "killing", "complex", "houses",
+//     "married", "kittens", "and", "single",
+//     "soldiers", "and", "their", "kleptomaniacal",
+//     "families"
+// ]
+
+// const filterWordsfunc = () => {
+//     const doesNotStartWithK = []
+//     for (const wordsStr of words) {
+//         if (wordsStr.startsWith("k") === false) {
+//             doesNotStartWithK.push(wordsStr)
+//         }
+//     }
+//     return doesNotStartWithK.join(" ")
+// }
+
+// console.log(filterWordsfunc())
+
+
+
+// const tunaFish = () => {
+//     randomMath = Math.random() * 3.33
+//     console.log(randomMath)
+//     if (randomMath >= 2) {
+//         console.log("Sven hooked a tuna!  :)")
+//     } else {
+//         console.log("Sven came up empty-handed.  :(")
+//     }
+// }
+
+// tunaFish()
+
